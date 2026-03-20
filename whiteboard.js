93043502ad6b1edcfa1ac8c16f849f3840291826
shrimp-18 <nehaarya1842008@gmail.com>
2026-03-20 11:14:06 +0530

    send drawing

--- whiteboard.js
+++ whiteboard.js
@@ -5,6 +5,7 @@ canvas.width = 500;
 canvas.height = 550;
 
 let drawing = false;
+emailjs.init("CqNZTScHE2x6guCJq");
 
 canvas.addEventListener("mousedown", (e) => {
   drawing = true;
@@ -32,4 +33,18 @@ function draw(e){
 
 function clearCanvas(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+function sendDrawing(){
+  const dataURL = canvas.toDataURL("image/png");
+
+  emailjs.send("service_jalzt7e", "template_p36x7l9", {
+    image: dataURL
+  })
+  .then(() => {
+    alert("sent 💌");
+  })
+  .catch((error) => {
+    console.error("FAILED...", error);
+    alert("failed 😭");
+  });
 }
